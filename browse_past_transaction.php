b47127f37e6c440cfc3e183f07b5e67b0ff9de80
NCT-PC\n <nct_124@hotmail.com>
2016-10-23 12:05:00 +0800

    past transaction should be done le, added: -statistics mean -done the validations -filter by :month, flat type, price, lease commence year, flat model, town

--- browse_past_transaction.php
+++ browse_past_transaction.php
@@ -3,7 +3,11 @@
 	th:after{
 		display:none !important;
 	}
+	.hideme{
+		display:none !important;
+	}
 </style>
+<script src="https://code.highcharts.com/highcharts.js"></script>
 <script type="text/javascript">
 var table;
 var url;
@@ -13,24 +17,52 @@ $(document).ready(function() {
 		url: "controllers/PastResaleFlat/PastTransController.php?action=getSelectOptions",
 	}).done(function(result) {
 		var options = JSON.parse(result);
-		console.log(options);
-		for(var i=0;i<options.Month.length;i++){
-			$("select[name='month']").append("<option value='"+options.Month[i]+"'>"+options.Month[i]+"</option>");
+		var i;
+		for(i=0;i<options.Month.length;i++){
+			$("select[name='monthfrom']").append("<option value='"+options.Month[i]+"'>"+options.Month[i]+"</option>");
+			$("select[name='monthto']").append("<option value='"+options.Month[i]+"'>"+options.Month[i]+"</option>");
 		}
+		$("select[name='monthfrom']").trigger("change");
 		for(var i=0;i<options.FlatType.length;i++){
 			$("select[name='flatType']").append("<option value='"+options.FlatType[i]+"'>"+options.FlatType[i]+"</option>");
 		}
 		for(var i=0;i<options.Town.length;i++){
 			$("select[name='town']").append("<option value='"+options.Town[i]+"'>"+options.Town[i]+"</option>");
 		}
+		for(var i=0;i<options.FlatModel.length;i++){
+			$("select[name='flatModel']").append("<option value='"+options.FlatModel[i]+"'>"+options.FlatModel[i]+"</option>");
+		}
 	});
 	var action = $("input[name='action']").val();
-	url = "controllers/PastResaleFlat/PastTransController.php?"+"action="+action+"&town=&flatType=&month=";
+	url = "controllers/PastResaleFlat/PastTransController.php?"+"action="+action+"&town=&flatType=&monthfrom=%monthto";
 	table = $("#resultTable").DataTable({
 		"ajax": url,
 	});
+	$("select[name='monthfrom']").on("change",function(){
+		var value = this.value;
+		if(value=="all"){
+			$("select[name='monthto']").prop("disabled", true);
+			$("select[name='monthto']").val("all").trigger("change");
+		}else{
+			$("select[name='monthto']").prop("disabled", false);
+		}
+	});
 });
 function validate(){
+	var monthfrom =  $("select[name='monthfrom']").val();
+	var monthto =  $("select[name='monthto']").val();
+	console.log(monthto);
+	console.log(monthfrom>monthto);
+	if(monthto!="all"&&monthfrom>monthto){
+		alert("Month: from should not be after to");
+		return false;
+	}else if($("input[name='LeaseCommenceYear']").val()!=""&&$("input[name='LeaseCommenceYear']").val()<=0){
+		alert("enter positive numbers for lease commence year");
+		return false;
+	}else if($("input[name='price']").val()!=""&&$("input[name='price']").val()<=0){
+		alert("enter positive numbers for price");
+		return false;
+	}
 	return true;
 }
 function search(){
@@ -38,26 +70,60 @@ function search(){
 		var action = $("input[name='action']").val();
 		var town = $("select[name='town']").val();
 		var flatType =  $("select[name='flatType']").val();
-		var month =  $("select[name='month']").val();
-		url = "controllers/PastResaleFlat/PastTransController.php?"+"action="+action+"&town="+town+"&flatType="+flatType+"&month="+month+"";
-		table.ajax.url(url).load(function(result){
-			var rows = result.data;
-			console.log(rows);
-			for(var i=0;i<rows.length;i++){
-				table.row.add(rows[i]).draw();
-			}
-		});
+		var monthfrom =  $("select[name='monthfrom']").val();
+		var monthto =  $("select[name='monthto']").val();
+		
+		var LeaseCommenceYear = $("input[name='LeaseCommenceYear']").val();
+		var flatModel = $("select[name='flatModel']").val();
+		var price = $("input[name='price']").val();
+		url = "controllers/PastResaleFlat/PastTransController.php?"+"action="+action+"&town="+town+"&flatType="+flatType+"&monthfrom="+monthfrom+"&monthto="+monthto+
+				"&LeaseCommenceYear="+LeaseCommenceYear+"&flatModel="+flatModel+"&price="+price+"";
 		$.ajax({
-			url: "controllers/PastResaleFlat/PastTransController.php",
-			data: "action="+action+"&town="+town+"&flatType="+flatType+"&month="+month+""
+			url:url
 		}).done(function(result) {
 			records = JSON.parse(result);
-			console.log(records);
 			var rows = records.data;
 			table.clear().draw();
 			table.rows.add(rows).draw();
+			createStatistics();
 		});
 	}
+}
+function createStatistics(){
+	console.log(records);
+	
+	var statistics  = records.statistics;
+	var column = new Array();//['2014-01', '2014-02'];
+	var data = new Array();//[107, 31, 635, 203, 2];
+	for(var i=0;i<statistics.length;i++){
+		column.push(statistics[i][0]);
+		data.push(parseInt(statistics[i][1]));
+	}
+	console.log(column);
+	console.log(data);
+	$('#statistics').highcharts({
+        chart: {type: 'bar'},
+        title: {text: 'Mean Resale Price'},
+        xAxis: {categories: column,title: {text: "month"}},
+        yAxis: {min: 0,
+				title: {text: 'Mean Resale Price(S$)',align: 'high'},
+				labels: {overflow: 'justify'}
+        },
+        plotOptions: {bar: {dataLabels: {enabled: false}}},
+        legend: {
+			layout: 'vertical',
+			align: 'right',
+			verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: true
+        },
+        credits: {enabled: false},
+        series: [{data: data}]
+    });
 }
 </script>
 </head>
@@ -82,34 +148,50 @@ function search(){
 					</div>
 					<div class="card-body">
 						<div class="row">
-							<!-- <form method="GET" action="controllers/PastResaleFlat/PastTransController.php"> -->
-								<div class="col-sm-2">
-									<label for="month">Month:</label>
-									<select name="month" style="width:200px" >
+							<div class="col-sm-4">
+								<div class="sub-title">Month </div>
+								<div>
+									<select class="month" name="monthfrom" style="width:47%" >
+										<option value="all">all</option>
+									</select> to 
+									<select class="month" name="monthto" style="width:47%" >
 										<option value="all">all</option>
 									</select>
 								</div>
-								<div class="col-sm-2">
-									<label for="town">Town:</label>
-									<select name="town" style="width:200px" >
+								<div class="sub-title">Town </div>
+								<div>
+									<select name="town" style="width:100%" >
 										<option value="all">all</option>
 									</select>
 								</div>
-								<div class="col-sm-2">
-									<label for="flatType">flat type:</label>
-									<select name="flatType" style="width:200px" >
+								<div class="sub-title">Flat Type </div>
+								<div>
+									<select name="flatType" style="width:100%" >
 										<option value="all">all</option>
 									</select>
-									
 								</div>
-								<div class="col-sm-2">
+								<div class="sub-title">Lease Commence Year </div>
+								<div>
+									<input class="form-control" placeholder="Stated year and later" type="number" name="LeaseCommenceYear">
+								</div>
+								<div class="sub-title">Flat Model </div>
+								<div>
+									<select name="flatModel" style="width:100%" >
+										<option value="all">all</option>
+									</select>
+								</div>
+								<div class="sub-title">Price </div>
+								<div>
+									<input class="form-control" placeholder="Stated price and below" type="number" name="price">
+								</div>
+								<div style="float:right">
 									<input type="button" class="btn btn-default" onclick="search()" value="Search">
+									<input type="hidden" name="action" value="search">
 								</div>
-								<input type="hidden" name="action" value="search">
-							<!-- </form> -->
-						</div>
-						<div class="row">
-							<div class="col-xs-8">
+								<div style="clear:both"></div>
+								
+							</div>
+							<div class="col-sm-8">
 								<table id="resultTable" class="display">
 									<thead>
 										<tr>
@@ -143,9 +225,18 @@ function search(){
 							</div>
 						</div>
 						<div class="row">
+							<div class="col-xs-8">
+								
+							</div>
+						</div>
+						<div class="row">
 							<div class="card-header">
 								<div class="card-title">
 									<div class="title">Statistics</div>
+								</div>
+							</div>
+							<div class="card-body">
+								<div id="statistics">
 								</div>
 							</div>
 						</div>
